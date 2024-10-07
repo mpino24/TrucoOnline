06bd5566862bf12235a535f81db3f81eee70e0e6
paula <paulagonzalezllanes@gmail.com>
2024-10-07 10:38:11 +0200

    Index.Home creationModal
    Co-authored-by: Izshin <Izshin@users.noreply.github.com>

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import '../App.css';
 import '../static/css/home/home.css';
 import '../static/css/home/homeButton.css';
+import { Form, Label, Input } from "reactstrap";
 import { FaUserFriends } from "react-icons/fa";
 import { RiArrowRightDoubleLine } from "react-icons/ri";
 import { RiArrowLeftDoubleLine } from "react-icons/ri";
 import { IoTrophy } from "react-icons/io5";
-
-
+import {DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
 
 export default function Home() {
     const backgrounds= [
@@ -22,16 +22,20 @@ export default function Home() {
         'url(/fondos/fondo8.jpg)',
         'url(/fondos/fondo9.jpg)',
     ];
+    const [creationModal, setCreationModalView] = useState(false);
 
     const getRandomBackground = () => { //Generado por IA
         const randomIndex = Math.floor(Math.random() * backgrounds.length);
         return backgrounds[randomIndex];
     }
-       
-
-
+    const toggleCreationModal = useCallback(() => {
+        setCreationModalView((current) => !current);
+    }, []);
+    
     return (
-        <div className="home-page-container" style={{background: getRandomBackground(), backgroundSize:'cover'}}>
+
+        <div>
+          <div className="home-page-container" style={{background: getRandomBackground(), backgroundSize:'cover'}}>
             <div style={{width:'36%' }}>
                 <div style={{cursor:'pointer'}}>
                 <IoTrophy style={{ width: 80, height: 80, float: 'left',color:'white' }} />
@@ -40,11 +44,52 @@ export default function Home() {
             </div>
             </div>
 
-            <div className="hero-div">
+            
+          {creationModal &&  <div className="hero-div">
                 <h1>¿Un truco?</h1>
-                <button className="home-button">Crear</button>
+                <button onPress={()=>toggleCreationModal()} className="home-button">Crear</button>
                 <button className="home-button">Unirte</button>
-            </div>
+            </div> }
+
+
+             { !creationModal && <div className= 'cuadro-creacion'>
+             <Form>
+                <h3> Ajuste de la partida: </h3>
+                        <div>
+                            <Label>Numero de jugadores: 
+                            </Label>
+                            <DropdownItem>
+                            <DropdownToggle> . </DropdownToggle>             
+                            <DropdownMenu>
+                                <DropdownItem >2</DropdownItem>
+                                <DropdownItem >4</DropdownItem>
+                                <DropdownItem >6</DropdownItem>
+                            </DropdownMenu>
+                            </DropdownItem>
+
+                        </div>
+                        
+                        <Input type="checkbox" id="checkboxInput">
+                            <Label for="checkboxInput" class="toggleSwitch"> Numero de puntos para ganar:
+                            </Label>
+                        </Input>
+                        <div>
+                            <Label>
+                                Con Flor:
+                            </Label>
+                            </div>
+                            <div>
+                            <Label>
+                                Partida privada:
+                            </Label>
+                            </div>
+                            
+                            <div className="botones">
+                            <button onPress={()=>toggleCreationModal()} className="recuadro">Crear</button>
+                            <button className="recuadro">Volver</button>
+                            </div>
+                    </Form>
+            </div>}
 
             <div style={{ width:'36%' }}>
                 <div style={{cursor:'pointer'}} >
@@ -56,7 +101,8 @@ export default function Home() {
             <div style={{ backgroundColor: 'black', position: 'fixed', bottom: 0, width: '100%', height: 41 }}>
                 <center style={{ color: 'white', marginTop: 5 }}>© MIDPIE</center>
             </div>
-        </div>
+        </div> 
+    </div>
     );
 
 }
