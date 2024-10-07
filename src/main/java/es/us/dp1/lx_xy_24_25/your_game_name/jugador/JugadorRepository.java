0e4ecc51e53d78cd651a333dadf423c669c1946b
Ivo Raimondi <raimondiivo@gmail.com>
2024-10-07 19:14:39 +0200

    Jugador Repository (solo funciones CRUD)  y borrado de clase player Fixes #81

---
+++ src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/jugador/JugadorRepository.java
@@ -0,0 +1,10 @@
+package es.us.dp1.lx_xy_24_25.your_game_name.jugador;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JugadorRepository extends CrudRepository<Jugador,Integer>{
+    
+
+    
+
+}
