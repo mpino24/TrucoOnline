0e9d490b9ef7bddb5e6a2f8867d66ec4ac24616c
HMF2475 <raimondiivo@gmail.com>
2024-11-03 21:22:19 +0100

    Otros cambios NO SUBIR A MAIN

--- src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/TrucoBeastsInitializer.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/TrucoBeastsInitializer.java
@@ -3,11 +3,11 @@ package es.us.dp1.lx_xy_24_25.truco_beasts;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class GameInitializer extends SpringBootServletInitializer {
+public class TrucoBeastsInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GameApplication.class);
+		return application.sources(TrucoBeastsApplication.class);
 	}
 
 }
