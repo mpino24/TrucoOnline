403669d6185a41f7d982371713bf49a8e0db01af
paula <paulagonzalezllanes@gmail.com>
2025-01-04 19:28:16 +0100

    LogroService

---
+++ src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/estadisticas/LogrosService.java
@@ -0,0 +1,69 @@
+package es.us.dp1.lx_xy_24_25.truco_beasts.estadisticas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.us.dp1.lx_xy_24_25.truco_beasts.user.UserRepository;
+
+import jakarta.validation.Valid;
+
+@Service
+public class LogrosService {
+  
+    
+    private final LogroRepository logroRepository;
+
+    @Autowired
+    public LogrosService(LogroRepository logroRepository){
+        this.logroRepository= logroRepository;
+
+    }
+
+
+    @Transactional
+    public Logros save(Logros logro) throws DataAccessException{
+        logroRepository.save(logro);
+        return logro;
+    }
+
+    @Transactional
+    public Logros updateLogro(@Valid Logros logro){
+        Optional<Logros> logroToUpdate = logroRepository.findById(logro.getId());
+        if(logroToUpdate != null){
+            BeanUtils.copyProperties(logro, logroToUpdate, "id");
+        }
+        return save(logro);
+    }
+
+    @Transactional(readOnly=true)
+    public List<Logros> findAllLogros(){
+        List<Logros> res= new ArrayList<>();
+        logroRepository.findAll().forEach(x-> res.add(x));
+        return res;
+    }
+
+    @Transactional(readOnly = true)
+    public Logros findLogroByMetrica(Metrica metrica){
+        return logroRepository.findByMetrica(metrica);
+    }
+
+    @Transactional(readOnly = true)
+    public Logros findLogroById(Integer id){
+        return logroRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void deleteLogro(Integer logroId){
+        Logros logro = findLogroById(logroId);
+        logroRepository.delete(logro);
+    }
+
+
+}
