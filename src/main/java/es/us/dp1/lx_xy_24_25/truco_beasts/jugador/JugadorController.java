a688a7e0345d61d329f5e1e3b03e6370d84920f1
paula <paulagonzalezllanes@gmail.com>
2025-01-10 18:01:26 +0100

    Test JugadorController

--- src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/jugador/JugadorController.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/jugador/JugadorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +52,7 @@ public class JugadorController {
         Jugador res = jugadorService.updateJugador(jugador, userService.findUser(userId));
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
+
 
     @PostMapping("/edit")
     public ResponseEntity<Jugador> saveJugadorEdit(@RequestBody @Valid Jugador jugador, @RequestParam Integer userId) {
