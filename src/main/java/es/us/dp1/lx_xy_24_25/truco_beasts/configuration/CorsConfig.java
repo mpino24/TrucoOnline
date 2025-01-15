347021d977b94da234bf45cb21a763980c17f134
David <davidvt04@gmail.com>
2025-01-15 13:58:23 +0100

    Otra vez

--- src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/configuration/CorsConfig.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/truco_beasts/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://lively-dune-025787b1e.4.azurestaticapps.net")
+                        .allowedOrigins("http://localhost:3000","https://lively-dune-025787b1e.4.azurestaticapps.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
